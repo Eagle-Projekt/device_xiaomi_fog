50d40efa8d2d10fe48f3612d7ab868f2da5adc6e
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    fog: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints in the future
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Tanmsyffa <sltnmsyffaa@gmail.com>

--- device.mk
+++ device.mk
@@ -354,6 +354,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor \
