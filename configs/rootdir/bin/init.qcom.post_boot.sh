ef85703d2b0ef67080eb2d57fdcc7d3884bd9f9d
Jabiyeff <cebiyevanar@gmail.com>
2022-05-24 14:10:18 +0400

    fog: rootdir: Remove cpusets on post boot script
    * Them already defined on init.qcom.power.rc
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: Tanmsyffa <sltnmsyffaa@gmail.com>

--- configs/rootdir/bin/init.qcom.post_boot.sh
+++ configs/rootdir/bin/init.qcom.post_boot.sh
@@ -234,13 +234,6 @@ case "$soc_id" in
     echo 85 > /proc/sys/kernel/sched_group_downmigrate
     echo 100 > /proc/sys/kernel/sched_group_upmigrate
 
-    # cpuset settings
-    echo 0-2     > /dev/cpuset/background/cpus
-    echo 0-3     > /dev/cpuset/system-background/cpus
-    echo 4-7     > /dev/cpuset/foreground/boost/cpus
-    echo 0-2,4-7 > /dev/cpuset/foreground/cpus
-    echo 0-7     > /dev/cpuset/top-app/cpus
-
     # configure governor settings for little cluster
     echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
     echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
