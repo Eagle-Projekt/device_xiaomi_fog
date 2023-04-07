21e0dc6b15a060d26ad817e412ec3169d96b6592
Tanmsyffa <sltnmsyffaa@gmail.com>
2023-04-07 14:08:39 +0000

    fog: AOSP-ify

--- aosp_fog.mk
+++ aosp_fog.mk
@@ -8,13 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
+TARGET_BOOT_ANIMATION_RES := 720
 
-PRODUCT_NAME := lineage_fog
+PRODUCT_NAME := aosp_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
